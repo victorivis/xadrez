faf82e3ebaf02ef7e98b4976c235b1c5dadfb3e2
victorivis <knightdragonred@gmail.com>
2025-03-31 18:59:11 -0300

    versao texto basico

---
+++ Makefile
@@ -0,0 +1,22 @@
+# Diretório onde estão os arquivos fonte
+SRC_DIR = .
+
+# Diretório onde serão gerados os arquivos objeto
+OBJ_DIR = obj
+
+# Libs
+LIBS = -I ..\..\SDL2\x86_64-w64-mingw32\include\ -L ..\..\SDL2\x86_64-w64-mingw32\lib\ -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_image -DLOCAL
+WEBLIBS = -s WASM=1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_TTF=2 --preload-file assets -s EXIT_RUNTIME=1
+FILES = main.cpp pecas.cpp menu.cpp
+
+wincpp: $(FILES)
+	g++ $(FILES) $(LIBS) -o build/saida.exe
+
+webcpp: $(FILES)
+	em++ $(FILES) $(WEBLIBS) -o build/saida.html
+
+winrun: wincpp
+	build/saida.exe
+
+webrun: webcpp
+	emrun build/saida.html
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
+#include <cstdio>
+#include "menu.h"
+#include "pecas.h"
 
 #ifndef LOCAL
 
@@ -15,6 +18,10 @@ SDL_Event evento;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 TTF_Font * font;
+SDL_Color textColor;
+SDL_Surface *textSurface;
+SDL_Texture *textTexture;
+
 const int WIDTH = 1000;
 const int HEIGHT = 600;
 
@@ -42,6 +49,10 @@ void loopPrincipal(void* arg){
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
+
+    SDL_Rect textRect = {50, 50, textSurface->w, textSurface->h};
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
     SDL_RenderPresent(renderer);
 
     SDL_Delay(delay);
@@ -50,25 +61,41 @@ void loopPrincipal(void* arg){
 int main(int argc, char* argv[]){
     //Inicializando Janela
     SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
 	SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
     	
     font = TTF_OpenFont("assets/Sans.ttf", 25);
+    textColor = {0, 0, 0, 255};
+
+    if (font == NULL) {
+        printf("Erro ao carregar a fonte: %s\n", TTF_GetError());
+
+        int temp;
+        scanf("%d", &temp);
+    }
+
+    textSurface = TTF_RenderText_Solid(font, "Hello World!", textColor);
+    textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
 
     #ifndef LOCAL
         Context context;
         emscripten_set_main_loop_arg(loopPrincipal, &context, -1, 1);
     #else
+        int nada;
         while(rodar){
-            loopPrincipal();
+            loopPrincipal(&nada);
         }
     #endif    
 
     //Liberando memoria
+    SDL_DestroyTexture(textTexture);
+    SDL_FreeSurface(textSurface);
     TTF_CloseFont(font);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+    TTF_Quit();
 
     return 0;
 }
